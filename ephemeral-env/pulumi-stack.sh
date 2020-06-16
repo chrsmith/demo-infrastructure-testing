568a568be005f1eb11ecd01b649823376986380e
Angel Rivera <ariv3ra@gmail.com>
2020-06-16 18:16:47 -0400

    Cleaned up some output cmds from the pulumi-stack.sh file and added the destroy command to the pulumi infra stuff.

--- ephemeral-env/pulumi-stack.sh
+++ ephemeral-env/pulumi-stack.sh
@@ -4,7 +4,6 @@ STACK_NAME=$1
 
 TEST_STACK=$(pulumi stack select --stack $STACK_NAME 2>&1)
 NO_STACK="error: no stack named"
-echo "test_STACK var: "$TESTSTACK
 
 if [[ "$TEST_STACK" == *"$NO_STACK"* ]]; then
     echo "pulumi creating new stack $STACK_NAME"
